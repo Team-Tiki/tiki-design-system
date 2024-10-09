5f4be10d9611a1c0bfde0c4aa951eb67dbb2dd54
wuzoo <wndyd9911@gmail.com>
2024-10-09 21:05:24 +0900

    init: svg setting + svgr export

---
+++ packages/icon/generateIcon.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+const snakeToPascalCase = (fileName) => {
+    return fileName.split("_").map(str => {
+        const firstLetter = str.charAt(0).toUpperCase();
+
+        return firstLetter + str.slice(1);
+    }).join("");
+}
+
+const generate = () => {
+    const svgDir = path.join(__dirname, "src");
+    const outputDir = path.join(__dirname, "index.ts");
+
+    const files = fs.readdirSync(svgDir).map(svg => ({
+        file: svg,
+        fileName: snakeToPascalCase(path.basename(svg, ".svg"))
+    }))
+
+    fs.writeFileSync(outputDir, files.map(file => 
+        `export { default as ${file.fileName} } from "./src/${file.file}"`
+    ).join('\n'))
+}
+
+generate();

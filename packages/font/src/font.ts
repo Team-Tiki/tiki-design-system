471ae95996d2ba3b224798fc1772d7287ff3d2af
wuzoo <wndyd9911@gmail.com>
2024-10-12 16:54:56 +0900

    feat: apply generateFontObj func & rem -> px

--- packages/font/src/font.ts
+++ packages/font/src/font.ts
@@ -1,63 +1,29 @@
+import "../index.css";
+
+const generateFontObj = ({ fontSize, lineHeight }: { fontSize: string; lineHeight: string }) => ({
+  fontFamily: "'Pretendard Variable', Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif",
+  fontSize,
+  lineHeight,
+});
+
 const heading = {
-  heading01: {
-    fontSize: "4rem",
-    lineHeight: "4rem",
-  },
-  heading02: {
-    fontSize: "3.2rem",
-    lineHeight: "3.2rem",
-  },
-  heading03: {
-    fontSize: "2.8rem",
-    lineHeight: "2.8rem",
-  },
-  heading04: {
-    fontSize: "2.4rem",
-    lineHeight: "2.4rem",
-  },
-  heading05: {
-    fontSize: "2rem",
-    lineHeight: "2rem",
-  },
-  heading06: {
-    fontSize: "1.6rem",
-    lineHeight: "1.6rem",
-  },
+  heading01: generateFontObj({ fontSize: "40px", lineHeight: "40px" }),
+  heading02: generateFontObj({ fontSize: "32px", lineHeight: "32px" }),
+  heading03: generateFontObj({ fontSize: "28px", lineHeight: "28px" }),
+  heading04: generateFontObj({ fontSize: "24px", lineHeight: "24px" }),
+  heading05: generateFontObj({ fontSize: "20px", lineHeight: "20px" }),
+  heading06: generateFontObj({ fontSize: "16px", lineHeight: "16px" }),
 } as const;
 
 const text = {
-  body01: {
-    fontSize: "2.4rem",
-    lineHeight: "2.4rem",
-  },
-  body02: {
-    fontSize: "2.4rem",
-    lineHeight: "3.36rem",
-  },
-  body03: {
-    fontSize: "2rem",
-    lineHeight: "2rem",
-  },
-  body04: {
-    fontSize: "1.6rem",
-    lineHeight: "1.6rem",
-  },
-  body05: {
-    fontSize: "1.6rem",
-    lineHeight: "2.24rem",
-  },
-  body06: {
-    fontSize: "1.4rem",
-    lineHeight: "1.4rem",
-  },
-  body07: {
-    fontSize: "1.4rem",
-    lineHeight: "1.68rem",
-  },
-  body08: {
-    fontSize: "1.2rem",
-    lineHeight: "1.2rem",
-  },
+  body01: generateFontObj({ fontSize: "24px", lineHeight: "24px" }),
+  body02: generateFontObj({ fontSize: "24px", lineHeight: "33.6px" }),
+  body03: generateFontObj({ fontSize: "20px", lineHeight: "20px" }),
+  body04: generateFontObj({ fontSize: "16px", lineHeight: "16px" }),
+  body05: generateFontObj({ fontSize: "16px", lineHeight: "22.4px" }),
+  body06: generateFontObj({ fontSize: "14px", lineHeight: "14px" }),
+  body07: generateFontObj({ fontSize: "14px", lineHeight: "16.8px" }),
+  body08: generateFontObj({ fontSize: "12px", lineHeight: "12px" }),
 } as const;
 
 export type HeadingTypes = typeof heading;
